77d15e0adff37bf491d775e684df0c6c4f579a5b
17zhaoamy@gmail.com <17zhaoamy@gmail.com>
2019-03-30 17:29:34 -0700

    Mongodb

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/music";
+
+MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+	console.log("Database created!");
+	var dbo = db.db("music");
+	dbo.createCollection("songs", function(err, res) {
+		if(err) throw err;
+		console.log("Collection created!");
+	});
+});
+
 var express = require("express");
 
 var app = express();
